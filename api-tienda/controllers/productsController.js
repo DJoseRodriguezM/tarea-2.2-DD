ccafcb0cd77dcb5d1a8906136e4c28a1fe7d10c8
CarlosMartin2005 <cpenap@unah.hn>
2024-11-17 19:27:10 -0600

    getProductById

--- api-tienda/controllers/productsController.js
+++ api-tienda/controllers/productsController.js
@@ -33,10 +33,45 @@ export class ProductsController {
     }
 
     static getProductById(req, res) {
+        const { id } = req.params
+        const consulta = "SELECT * FROM productos WHERE id = ?"
 
+        try {
+            connection.query(consulta, [id], (error, results) => {
+
+                if (error) {
+                    return res.status(400).json({
+                        error: true,
+                        message: "Ocurrió un error al obtener los datos: " + error
+                    })
+                }
+
+                if (results && results.length === 0) {
+                    res
+                        .json({
+                            message: "Usuario no encontrado"
+                        })
+                }
+
+                return res
+                    .header('Content-Type', 'application/json')
+                    .status(200)
+                    .json(results)
+
+            })
+
+        } catch (error) {
+
+            return res.status(400).json({
+                error: true,
+                message: "Ocurrió un error al obtener los datos: " + error
+            })
+        
+        }
     }
 
     static createProduct(req, res) {
+        
         
     }
 
