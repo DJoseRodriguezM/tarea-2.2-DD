acc3796a86a8ea12fc595070a64b659fdb388619
CarlosMartin2005 <cpenap@unah.hn>
2024-11-17 20:23:11 -0600

    getCartByUser

--- api-tienda/controllers/cartController.js
+++ api-tienda/controllers/cartController.js
@@ -3,7 +3,30 @@ import connection from '../config/db.js'
 
 export class CartController {
     static getCartByUser(req, res) {
+    const { userId } = req.params
 
+    const consulta = "SELECT * FROM carrito WHERE id = ?"
+
+    try {
+        connection.query(consulta, [userId], (error, results) => {
+
+            if (error) {
+                return res.status(400).json({
+                    error: true,
+                    message: "Ocurrió un error al obtener los datos: " + error
+                })
+            }
+            return res
+                .header('Content-Type', 'application/json')
+                .status(200)
+                .json(results)
+        })
+    } catch (error) {
+        return res.status(400).json({
+            error: true,
+            message: "Ocurrió un error al obtener los datos" + error
+        })
+    }
     }
 
     static addToCart(req, res) {
